a536eec893495aadb7fcd575739fbbb497084d73
yasmine <mansour.yasmine@esprit.tn>
2024-04-22 16:29:41 +0100

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AllTemplatesBackComponent } from './BackOffice/all-templates-back/all-templates-back.component';
+import { AllTemplatesFrontComponent } from './FrontOffice/all-templates-front/all-templates-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { HeaderFrontComponent } from './FrontOffice/header-front/header-front.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CollocationListComponent } from './FrontOffice/viewsFront/colocation/collocation-list/collocation-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AllTemplatesBackComponent,
+    AllTemplatesFrontComponent,
+    FooterFrontComponent,
+    HeaderFrontComponent,
+    NavbarBackComponent,
+    SidebarBackComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
