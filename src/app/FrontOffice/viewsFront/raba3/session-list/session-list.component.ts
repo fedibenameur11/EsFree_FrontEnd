0eaed8b0c8293f8647d8724ea0743747f82af772
Farah Diouani <farah.diouani@esprit.tn>
2024-05-07 15:29:44 +0100

    ya allah

--- src/app/FrontOffice/viewsFront/raba3/session-list/session-list.component.ts
+++ src/app/FrontOffice/viewsFront/raba3/session-list/session-list.component.ts
@@ -6,7 +6,6 @@ import Swal from 'sweetalert2';
 import { MatDialog } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { take } from 'rxjs';
-import { AngularFireMessaging } from '@angular/fire/messaging';
 
 
 
@@ -29,7 +28,7 @@ export class SessionListComponent implements OnInit{
     this.showAddDialog = false;
   }
   
-  constructor(private afMessaging: AngularFireMessaging,private act:ActivatedRoute,private raba3Service:Raba3Service , private router: Router,private dialog :MatDialog){ }
+  constructor(private act:ActivatedRoute,private raba3Service:Raba3Service , private router: Router,private dialog :MatDialog){ }
   public sessions: Array<Raba3> =[];
 
 
@@ -103,51 +102,6 @@ export class SessionListComponent implements OnInit{
     
       // Return an object containing the hours and minutes
       return { hours, minutes: remainingMinutes };
-    }
-
-
-    sendNotification(): void {
-      this.afMessaging.requestToken.pipe(take(1)).subscribe(
-        token => {
-          // Send notification using token
-          this.sendNotificationToToken(token);
-        },
-        error => {
-          console.error('Error requesting token:', error);
-        }
-      );
-    }
-  
-    private sendNotificationToToken(token: string): void {
-      // Replace with your notification payload
-      const notificationPayload = {
-        token,
-        to : "eIpYA7Puv0NqXUTIKlKwcf:APA91bFfj5CbK_eEbIH7Xplnz8qPtLjt8k_f3tlgt4m2c-0OMzrlnJAbuwIIZLJzBpbi4r8zTKYwFJZsC0NyU2B1EqxL-yeS-3yvVQv0Bm9Wx1R8WNQU_zu7Y3MKUR4G8z2iE9gXsBZQ",
-        notification: {
-        title : "Game Session",
-        body : "A user joined your session",
-        icon : "https://i.ibb.co/zWgfqtD/430112506-957916262375552-2785303533872510482-n.jpg"
-        }
-      };
-  
-      // Send notification to the device with the specified token
-      // Replace 'YOUR_SERVER_KEY' with your Firebase Cloud Messaging Server Key
-      fetch('https://fcm.googleapis.com/fcm/send', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'key=AAAAVhaWNgw:APA91bFggg2WnoXuzUZP5SyCgYF3G8xi4j455wRoz5-rPOulMWDTonUiJAMsnyvDHFAJlyt376rm59HBbudITMBtMYm7_OWec8gf2lXnbbuRBHRxscA53XB7MJN_NiZp3mmOolQbrjzF' // Replace with your Firebase Cloud Messaging Server Key
-        },
-        body: JSON.stringify(notificationPayload)
-      })
-      .then(response => {
-        console.log('Notification sent successfully:', response);
-        // Add visual feedback for successful notification
-      })
-      .catch(error => {
-        console.error('Error sending notification:', error);
-        // Add visual feedback for failed notification
-      });
     }
   }
     
